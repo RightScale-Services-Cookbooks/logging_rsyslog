899672073d68d079e9a29182caea3c9ae2f82f55
Alex Pop <alex@rightscale.com>
2014-04-02 16:02:49 +0100

    Update add_compress_logs_cronjob.rb

--- recipes/add_compress_logs_cronjob.rb
+++ recipes/add_compress_logs_cronjob.rb
@@ -5,8 +5,8 @@
 rightscale_marker :begin
 
 # Compress *.log files not changed in more than 24 hours
-template "/etc/cron.hourly/syslog-bzip2" do
-  source "syslog-bzip2.erb"
+template "/etc/cron.hourly/syslog-zip-logs" do
+  source "syslog-zip-logs.erb"
   mode 0755
   variables(
     :path => "/mnt/ephemeral/syslog"
