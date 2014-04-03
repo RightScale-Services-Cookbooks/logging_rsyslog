5e3c6731649dcb5c8a8a0352756d8cbd2a538d91
Alex Pop <alex@rightscale.com>
2014-04-03 19:13:56 +0100

    Update backup_logs_schedule_disable.rb

--- recipes/backup_logs_schedule_disable.rb
+++ recipes/backup_logs_schedule_disable.rb
@@ -3,7 +3,7 @@ rightscale_marker :begin
 cron "rs_syslog_backup" do
   minute "15"
   hour "0"
-  command "rs_run_recipe --name 'openvpn::backup_logs' 2>&1 >> /var/log/rs_backup.log"
+  command "rs_run_recipe --name 'logging_rsyslog::backup_logs' 2>&1 >> /var/log/rs_backup.log"
   action :delete
 end
 
