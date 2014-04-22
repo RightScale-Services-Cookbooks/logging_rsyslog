b9a9d2245eb45a0933f88eab79a82c0f4d6451a6
Alex Pop <alex@rightscale.com>
2014-04-22 13:13:53 +0100

    Update backup_logs_schedule_enable.rb

--- recipes/backup_logs_schedule_enable.rb
+++ recipes/backup_logs_schedule_enable.rb
@@ -3,7 +3,7 @@ rightscale_marker :begin
 cron "rs_syslog_backup" do
   minute "45"
   hour "0"
-  command "rs_run_recipe --name 'logging_rsyslog::backup_logs' 2>&1 >> /var/log/rs_backup.log"
+  command "rs_run_recipe --name 'logging_rsyslog::backup_logs' 2>&1 >> /var/log/rs_syslog_backup.log"
   action :create
 end
 
